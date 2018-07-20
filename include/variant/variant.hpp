0502c3a4617258bb3e9247c1ed60555261d986a1
Greg Beard <greg.beard@vodafone.com>
2018-07-20 07:19:33 +0100

    Added `visit` overloads and copy ctor

--- include/variant/variant.hpp
+++ include/variant/variant.hpp
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <cassert>
+#include <type_traits>
 
 namespace variant {
 
@@ -33,6 +34,36 @@ namespace variant {
     template<size_t N, typename U>
     struct type_index_of<N, U> { };
 
+    template<typename... Ts>
+    struct all_noexcept_move_constructible;
+
+    template<typename T, typename... Ts>
+    struct all_noexcept_move_constructible<T, Ts...> {
+        static constexpr bool value = 
+            noexcept(T { std::declval<T>() }) &&
+                all_noexcept_move_constructible<Ts...>::value;
+    };
+
+    template<>
+    struct all_noexcept_move_constructible<> {
+        static constexpr bool value = true;
+    };
+
+    template<typename... Ts>
+    struct all_noexcept_copy_constructible;
+
+    template<typename T, typename... Ts>
+    struct all_noexcept_copy_constructible<T, Ts...> {
+        static constexpr bool value = 
+            noexcept(T { std::declval<T const&>() }) &&
+                all_noexcept_copy_constructible<Ts...>::value;
+    };
+
+    template<>
+    struct all_noexcept_copy_constructible<> {
+        static constexpr bool value = true;
+    };
+
     template<typename T, typename F, typename S, typename... Ts>
     auto apply_visitor(F&& f, S storage, Ts&&... args) {
         return 
@@ -42,8 +73,18 @@ namespace variant {
     template<typename... Ts>
     struct Variant {
 
-        template<typename U>
-        explicit Variant(U&& val) :
+        static_assert(0 < sizeof...(Ts),
+            "Variant must hold at least one type");
+
+        template<
+            typename U,
+            typename std::enable_if<
+                !std::is_same<
+                    typename std::decay<U>::type,
+                    Variant>::value>::type* = nullptr>
+        explicit Variant(U&& val)
+            noexcept(noexcept(typename std::decay<U>::type { std::declval<U>() }))
+        :
             type_index_ { 
                 type_index_of<0, typename std::decay<U>::type, Ts...>::value 
             } 
@@ -51,27 +92,28 @@ namespace variant {
             new (storage_) typename std::decay<U>::type { std::forward<U>(val) };
         }
 
-        Variant(Variant const&) = delete;
+        Variant(Variant const& other)
+            noexcept(all_noexcept_move_constructible<Ts...>::value)
+        :
+            type_index_ { other.type_index_ }
+        {
+            visit(
+                [this](auto const& val) {
+                    using T = typename std::decay<decltype(val)>::type;
+                    new (storage_) T { val };
+                },
+                other
+            );
+        }
 
         ~Variant() {
-            //  We need *visitors* for this. Can we implement this
-            //  using function pointers for the different types...
-            //  ```
-            //  auto f = [call_visitor<Ts>...];
-            //  f[type_index_](visitor_imp{}, storage_);
-            //  ```
-            auto destructor = [](auto& val) {
-                using T = typename std::decay<decltype(val)>::type;
-                val.~T();
-            };
-
-            using Fn = void (*)(decltype(destructor)&&, decltype(&storage_[0]));
-
-            Fn paths[sizeof...(Ts)] = {
-                apply_visitor<Ts, decltype(destructor)>...
-            };
-
-            (paths[type_index_])(std::move(destructor), &storage_[0]);
+            visit(
+                [](auto& val) {
+                    using T = typename std::decay<decltype(val)>::type;
+                    val.~T();
+                },
+                *this
+            );
         }
 
         Variant& operator=(Variant const&) = delete;
@@ -79,10 +121,27 @@ namespace variant {
         template<typename T, typename... Ts>
         friend auto is_alternative(Variant<Ts...> const&) -> bool;
 
+        template<typename F, typename T, typename... Ts>
+        friend auto visit(F&&, Variant<T, Ts...> const&) 
+            -> typename std::result_of<F(T const&)>::type;
+
+        template<typename F, typename T, typename... Ts>
+        friend auto visit(F&&, Variant<T, Ts...>&) 
+            -> typename std::result_of<F(T&)>::type;
+
     private:
+
         using Storage = 
             typename std::aligned_storage<max_size<Ts...>(),
                                            max_align<Ts...>()>::type;
+        auto get_storage() const -> Storage const* {
+            return &storage_[0];
+        }
+
+        auto get_storage() -> Storage* {
+            return &storage_[0];
+        }
+
         size_t type_index_;
         Storage storage_[1];
     };
@@ -90,6 +149,38 @@ namespace variant {
     template<typename T, typename... Ts>
     auto is_alternative(Variant<Ts...> const& v) -> bool {
         return type_index_of<0, T, Ts...>::value == v.type_index_;
+    }
+
+    template<typename F, typename T, typename... Ts>
+    auto visit(F&& visitor, Variant<T, Ts...> const& var) 
+        -> typename std::result_of<F(T const&)>::type
+    {
+        using Fr = typename std::add_rvalue_reference<F>::type;
+        using Fn = void (*)(Fr, decltype(var.get_storage()));
+        Fn paths[sizeof...(Ts) + 1] = {
+            apply_visitor<T const, Fr, decltype(var.get_storage())>,
+            apply_visitor<Ts const, Fr, decltype(var.get_storage())>...
+        };
+
+        return (paths[var.type_index_])(
+            std::forward<F>(visitor), 
+            var.get_storage());
+    }
+
+    template<typename F, typename T, typename... Ts>
+    auto visit(F&& visitor, Variant<T, Ts...>& var) 
+        -> typename std::result_of<F(T&)>::type
+    {
+        using Fr = typename std::add_rvalue_reference<F>::type;
+        using Fn = void (*)(Fr, decltype(var.get_storage()));
+        Fn paths[sizeof...(Ts) + 1] = {
+            apply_visitor<T, Fr, decltype(var.get_storage())>,
+            apply_visitor<Ts, Fr, decltype(var.get_storage())>...
+        };
+
+        return (paths[var.type_index_])(
+            std::forward<F>(visitor), 
+            var.get_storage());
     }
 }
 #endif //VARIANT_VARIANT_HPP_INCLUDED
--- tests/variant_tests.cpp
+++ tests/variant_tests.cpp
@@ -1,6 +1,8 @@
 #include "variant/variant.hpp"
 #include <string>
 #include <stdexcept>
+#include <type_traits>
+#include <iostream>
 
 #define TO_STR_IMPL(x) #x
 #define TO_STR(x) TO_STR_IMPL(x)
@@ -22,11 +24,11 @@ constexpr auto get_type_index(variant::Variant<Ts...> const&) -> size_t {
 
 struct A {
 
-    explicit A(bool* flag) :
+    explicit A(bool* flag) noexcept :
         flag_ { flag }
     { }
 
-    A(A&& other) :
+    A(A&& other) noexcept :
         flag_ { other.flag_ }
     {
         other.flag_ = nullptr;
@@ -45,21 +47,63 @@ private:
     bool* flag_;
 };
 
-auto main(int, char const**) -> int {
+auto type_index_tests() {
 
+    using MyVariant = variant::Variant<int, A, std::string>;
+    MyVariant v { 42 };
+    ENSURE(get_type_index<int>(v) == 0);
+    ENSURE(get_type_index<A>(v) == 1);
+    ENSURE(get_type_index<std::string>(v) == 2);
+}
+
+auto is_alternative_tests() {
+
+    using MyVariant = variant::Variant<int, A, std::string>;
+    MyVariant v { std::string { } };
+    ENSURE(variant::is_alternative<std::string>(v));
+    ENSURE(!variant::is_alternative<A>(v));
+}
+
+auto destructor_called_tests() {
     using MyVariant = variant::Variant<int, A, std::string>;
 
     bool destructor_called = false;
     {
         MyVariant v { A { &destructor_called } };
-        ENSURE(get_type_index<int>(v) == 0);
-        ENSURE(get_type_index<A>(v) == 1);
-        ENSURE(get_type_index<std::string>(v) == 2);
         ENSURE(variant::is_alternative<A>(v));
         ENSURE(!variant::is_alternative<std::string>(v));
     }
 
     ENSURE(destructor_called);
+}
 
+auto noexcept_constructor_tests() {
+    using MyVariant = variant::Variant<int, A, std::string>;
+    ENSURE(noexcept(std::string { }));
+    ENSURE(!noexcept(std::string { std::declval<std::string const&>() }));
+    ENSURE(noexcept(MyVariant {std::declval<std::string>()}));
+    ENSURE(!noexcept(MyVariant { std::declval<std::string const&>()}));
+}
+
+auto copy_construct_tests() {
+    using MyVariant = variant::Variant<int, float, std::string>;
+    MyVariant a { 42 };
+    MyVariant b = a;
+    ENSURE(variant::is_alternative<int>(b));
+}
+
+auto main(int, char const**) -> int {
+
+    try {
+        type_index_tests();
+        is_alternative_tests();
+        destructor_called_tests();
+        noexcept_constructor_tests();
+        copy_construct_tests();
+    }
+    catch(std::exception const& e) {
+        std::cerr << e.what() << "\n";
+        return -1;
+    }
     return 0;
 }
